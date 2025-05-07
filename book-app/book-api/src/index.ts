8872cbb8fff5c09b87b327daf2320689cd1339a7
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-05-07 10:24:57 +0200

    feat: setup express server with mongoDB-connection and middleware
    Co-Authored-By: Wiktor Okonski <159134437+PotoVic@users.noreply.github.com>
    Co-Authored-By: Valentin Björkli Castillo <valentin.bjorkli@outlook.com>

---
+++ book-app/book-api/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "*",
+    credentials: true
+}));
+
+mongoose.connect(process.env.MONGODB_URL || "");
+
+const PORT = 3000
+app.listen(PORT, () =>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
+
