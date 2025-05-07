69319a2e22f8ef038be10e1a488464b026b0b996
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-05-07 14:06:05 +0200

    feat: deleteBook now deletes the book's reviews

--- book-app/book-api/src/controllers/bookController.ts
+++ book-app/book-api/src/controllers/bookController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import Book from '../models/Book';
+import Review from '../models/Review';
 
 export const getAllBooks = async (req: Request, res: Response) => {
 	try {
@@ -116,6 +117,28 @@ export const updateBook = async (req: Request, res: Response) => {
 		}
 
 		res.status(200).json({ message: 'Book is now updated.', id: parseInt(id) });
+	} catch (error: unknown) {
+		const message = error instanceof Error ? error.message : 'Unknown error';
+		res.status(500).json({ error: message });
+	}
+};
+
+export const deleteBook = async (req: Request, res: Response) => {
+	try {
+		const id = req.params.id;
+		const book = await Book.findById(id);
+
+		if (!book) {
+			res.status(404).json({ message: 'Book not found. Nothing was deleted' });
+			return;
+		}
+
+        // Delete the reviews that can be found in book (book.reviews)
+        const deletedReviews = await Review.deleteMany({_id: {$in: book.reviews}});
+
+        const deletedBook = await Book.deleteOne({_id: id});
+
+		res.status(200).json({ message: 'Book is now deleted', data: {deletedBook, deletedReviews} });
 	} catch (error: unknown) {
 		const message = error instanceof Error ? error.message : 'Unknown error';
 		res.status(500).json({ error: message });
