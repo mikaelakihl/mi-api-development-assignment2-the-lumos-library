be598508d716bea1c6e29f07a30862c92dd25d68
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-05-07 18:46:09 +0200

    feat: added verifyToken for updateUser and deleteUser

--- book-app/book-api/src/routes/userRouter.ts
+++ book-app/book-api/src/routes/userRouter.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { deleteUser, fetchAllUsers, fetchOneUser, updateUser } from '../controllers/userController';
+import { loginUser } from '../controllers/authController';
+import { verifyAccessToken } from '../middleware/verifyToken';
 
 const router = express.Router()
 
 router.get('/', fetchAllUsers)
 router.get('/:id', fetchOneUser) 
-router.patch('/:id', updateUser) 
-router.delete('/:id', deleteUser)
+router.patch('/:id', verifyAccessToken, updateUser); 
+router.delete('/:id', verifyAccessToken,deleteUser);
 
 export default router;
