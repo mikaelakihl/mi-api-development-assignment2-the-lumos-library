eeab5a17367dbd7f8336341cf6010c25b39a5d31
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-05-07 12:24:40 +0200

    feat: add getAllBooks controller
    With search and sort support

--- book-app/book-api/src/controllers/bookController.ts
+++ book-app/book-api/src/controllers/bookController.ts
@@ -1,3 +1,32 @@
 import { Request, Response } from "express";
 import Book from '../models/Book'
 
+export const getAllBooks = async (req: Request, res: Response) => {
+    try {
+        const search = req.query.search;
+		const sort = req.query.sort;
+
+		let searchQuery: any = {};
+		let sortQuery = {};
+
+		if (search) {
+			searchQuery.content = { $regex: search, $options: 'i' };
+		}
+
+		let query = Book.find(searchQuery);
+
+		if (sort) {
+			const orderBy = sort === 'desc' ? -1 : 1;
+			sortQuery = { content: +orderBy };
+			query = Book.find(searchQuery).sort(sortQuery);
+		}
+
+		const books = await query;
+
+		res.json(books);
+    } catch (error: unknown) {
+		const message = error instanceof Error ? error.message : 'Unknown error';
+		res.status(500).json({ error: message });
+	}
+}
+
