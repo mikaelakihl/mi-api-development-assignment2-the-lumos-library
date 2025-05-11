72eac31880452322262d05cd826bbee930736ea4
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-05-11 12:56:45 +0200

    feat: add fetch of books

--- book-app/book-client/src/views/HomeView.vue
+++ book-app/book-client/src/views/HomeView.vue
@@ -1,5 +1,29 @@
 <script setup>
 
+import { onMounted, ref } from 'vue';
+
+const API_URL = import.meta.env.VITE_API_URL;
+const books = ref([]);
+// const searchQuery = ref('');
+
+const fetchBooks = async () => {
+  try {
+    const response = await fetch(API_URL + 'books');
+    
+    if(!response.ok) {
+      throw new Error(`HTTP-error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    books.value = data;
+  }catch(error) {
+    console.error('Error fetching books: ' + error) //TODO: Add better error-handling
+  }
+}
+
+onMounted(fetchBooks);
+
 </script>
 
 <template>
