6ece260363c583ca7bbf92a9e1302294f4458947
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-05-11 12:57:45 +0200

    feat: add html to print out books and relevant info

--- book-app/book-client/src/views/HomeView.vue
+++ book-app/book-client/src/views/HomeView.vue
@@ -27,10 +27,39 @@ onMounted(fetchBooks);
 </script>
 
 <template>
-  <h1>home</h1>
+	<main>
+		<section class="hero-section">
+			<h1>Legends awaits to be read<br />- explore enchanted tales</h1>
+			<span class="material-symbols-outlined"> keyboard_arrow_down </span>
+			<img />
+		</section>
+		<section>
+			<h2>Books</h2>
+			<form>
+				<input
+					type="search"
+					id="search-input"
+					name="search"
+					placeholder="Search..."
+					required
+					aria-label="Search for books"
+				/>
+				<button type="submit">
+          <span class="material-symbols-outlined"> search </span>
+        </button>
+			</form>
+      <div class="book-list" id="book-list" v-for="book in books" :key="book._id">
+        <article>
+          <h3>{{ book.title }}</h3>
+          <p>{{ book.author }}</p>
+          <p>{{ book.published_year }}</p>
+          <li v-for="(genre, i) in book.genres" :key="i">
+            {{ genre }}
+          </li>
+        </article>
+      </div>
+		</section>
+	</main>
 </template>
 
-<style lang="scss">
-
-
-</style>
+<style lang="scss"></style>
