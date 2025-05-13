b9083a67844611e166e64a7a581d1f75a4a9398d
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-05-13 16:17:07 +0200

    feat: added registerView and used AuthView to make the page look the same

---
+++ book-app/book-client/src/views/RegisterView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router';
+import AuthView from '../components/AuthView.vue'
+
+
+</script>
+
+<template>
+        <AuthView
+            :title="'Create new account'"
+            :submitText="'Create'"
+            :onSubmitHandler="login"
+            :errorMessage="error"
+            @update:username="username = $event"
+            @update:password="password = $event"
+        >
+            <!-- <button class="authview-form-button" type="button" @click="router.push('/register')">Register</button> -->
+            
+
+            <!-- <template #loginWithoutSignInLink>
+                <div class="authview-sign-in-without-login-wrapper">
+                    <RouterLink to ="/home" class="authview-form-button">Continue without signing in</RouterLink>
+                </div>
+            </template> -->
+        </AuthView>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
