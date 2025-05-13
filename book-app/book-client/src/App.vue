f76c8140172bc68d4ebdf82c442582aa774138da
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-05-13 10:52:36 +0200

    feat: set up menu to be ready for mobile and desktop versions

--- book-app/book-client/src/App.vue
+++ book-app/book-client/src/App.vue
@@ -1,9 +1,35 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import MenuDesktop from './fixtures/MenuDesktop.vue';
+import MenuMobile from './fixtures/MenuMobile.vue';
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const isMobile = ref(window.innerWidth <= 767);
+console.log('Is mobile?: ' + isMobile.value);
+
+function updateWindowResize() {
+  isMobile.value = window.innerWidth <= 767;
+  console.log('Is mobile?:' + isMobile.value);
+}
+
+function setupResizeListener() {
+	window.addEventListener('resize', updateWindowResize);
+}
+
+function removeResizeListener() {
+	window.removeEventListener('resize', updateWindowResize);
+}
+
+onMounted(setupResizeListener);
+onUnmounted(removeResizeListener);
 
 </script>
 
 <template>
+  <div>
+    <MenuDesktop v-if="isMobile" />
+    <MenuMobile v-else />
+  </div>
   <RouterView />
   
 </template>
