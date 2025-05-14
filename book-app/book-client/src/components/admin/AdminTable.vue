dc8be438f2c6b5ffbf80b99428f7b0036754bb53
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-05-14 17:43:38 +0200

    feat: add table component for admin-panel

---
+++ book-app/book-client/src/components/admin/AdminTable.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { computed, onMounted, ref } from 'vue';
+
+const props = defineProps({
+	tableHeaderOne: String,
+	tableHeaderTwo: String,
+	tableHeaderThree: String,
+	tableHeaderFour: String,
+	dbTableName: String,
+});
+
+const tableHeaders = computed(() =>
+    [props.tableHeaderOne, props.tableHeaderTwo, props.tableHeaderThree, props.tableHeaderFour]
+);
+
+const API_URL = import.meta.env.VITE_API_URL;
+const fetchedData = ref([]);
+
+const fetchData = async () => {
+	try {
+		if (!props.dbTableName) {
+			throw new Error('Missing dbTableName prop.');
+		}
+		const URL = `${API_URL}${props.dbTableName}`;
+        console.log('URL: ' +  URL.value);
+		const response = await fetch(URL);
+
+		if (!response.ok) {
+			throw new Error(`HTTP-error: ${response.status}`);
+		}
+
+		const data = await response.json();
+		fetchedData.value = data;
+	} catch (error) {
+		console.error('Error fetching data: ' + error); //TODO: Add better error-handling
+	}
+};
+
+function capitalizeHeader(header) {
+    if(!header) {
+        return '';
+    }
+    const newHeader = header.charAt(0).toUpperCase() + header.slice(1);
+    return newHeader;
+}
+
+onMounted(fetchData);
+</script>
+
+<template>
+	<table>
+		<thead>
+			<tr>
+				<th v-for="column in tableHeaders" :key="column">
+					{{ capitalizeHeader(column) }}
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="(item, index) in fetchedData" :key="index">
+				<td v-for="column in tableHeaders" :key="column">
+					{{ item[column] }}
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</template>
+
+<style lang="scss"></style>
