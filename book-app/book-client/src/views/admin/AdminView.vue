b028d2aa0ff0ae9dac3add0b06113f05e7fadb6b
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-05-14 15:01:55 +0200

    feat: add html for admin-dashboard

--- book-app/book-client/src/views/admin/AdminView.vue
+++ book-app/book-client/src/views/admin/AdminView.vue
@@ -1,5 +1,29 @@
-<script setup></script>
+<script setup>
+import AdminMenu from '@/components/admin/AdminMenu.vue';
+import { RouterLink } from 'vue-router';
 
-<template></template>
+</script>
 
-<style lang="scss" scoped></style>
+<template>
+	<div class="admin-container">
+        <AdminMenu />
+		<section class="admin-content">
+			<h2>Admin Dashboard</h2>
+			<div>
+				<h3>What do you want to see?</h3>
+				<RouterLink to="/admin-users">Users</RouterLink>
+				<RouterLink to="/admin-books">Books</RouterLink>
+			</div>
+		</section>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.admin-container {
+	display: grid;
+	grid-template-columns: repeat(10, 1fr);
+	.admin-content {
+		grid-column: 3 / span 8;
+	}
+}
+</style>
