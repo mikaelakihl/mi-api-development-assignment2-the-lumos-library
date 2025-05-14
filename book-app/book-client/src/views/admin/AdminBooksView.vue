a258c5d0f63b87210824e60645ca8d14b9d4555e
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-05-14 18:27:56 +0200

    feat: add bookView for admin-panel

--- book-app/book-client/src/views/admin/AdminBooksView.vue
+++ book-app/book-client/src/views/admin/AdminBooksView.vue
@@ -1,5 +1,50 @@
-<script setup></script>
+<script setup>
+import AdminMenu from '@/components/admin/AdminMenu.vue';
+import AdminTable from '@/components/admin/AdminTable.vue';
+import { RouterLink } from 'vue-router';
 
-<template></template>
+</script>
 
-<style lang="scss" scoped></style>
+<template>
+	<div class="admin-container">
+        <AdminMenu />
+		<section class="admin-content">
+			<h2>Books</h2>
+            <div class="table-container">
+                <AdminTable 
+                    dbTableName="books"
+                    tableHeaderOne="title"
+                    tableHeaderTwo="author"
+                    tableHeaderThree="genres"
+                    tableHeaderFour="created_at"
+                />
+            </div>
+		</section>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.admin-container {
+	display: grid;
+	grid-template-columns: repeat(12, 1fr);
+    background-color: $color-primary;
+    min-height: 100dvh;
+	.admin-content {
+		grid-column: 3 / span 10;
+        min-height: 90dvh;
+        margin: 1rem;
+        background-color: $color-background;
+        border-radius: 16px;
+        h2 {
+            font-family: $font-title;
+            font-size: $h-medium;
+            margin-top: 3rem;
+            margin-left: 2rem;
+            margin-bottom: 1rem;
+        }
+	}
+    .table-container {
+        grid-column: 3 / span 10;
+    }
+}
+</style>
